b0809c9b21b06b9f436ec7755fd98e2b83bfb4a9
annac777 <xc2736@nyu.edu>
2024-05-29 10:44:51 -0400

    finished version

--- solutions.py
+++ solutions.py
@@ -11,7 +11,9 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
-
+    total_sales = float(input("What is your projected amount of total sales? "))
+    profit = total_sales * 0.23
+    print("Profit: ${:,.2f}".format(profit))
 
 def calculate_quotient_and_remainder():
     """
@@ -22,6 +24,11 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
+    number1 = int(input("Enter number #1: "))
+    number2 = int(input("Enter number #2: "))
+    quotient = number1 // number2
+    remainder = number1 % number2
+    print(str(number2) + " goes into " + str(number1) + " a total of " + str(quotient) + " times with a remainder of " + str(remainder))
 
 
 def calculate_miles_per_gallon():
@@ -35,7 +42,12 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
-
+    miles_driven = float(input("What is the number of miles driven? "))
+    gallons_used = float(input("What is the gallons of gas used? "))
+    mpg = miles_driven / gallons_used
+    print("Miles driven: " + str(int(miles_driven)))
+    print("Gas used (gallons): " + str(int(gallons_used)))
+    print("Miles per gallon: " + str(mpg))
 
 def align_text():
     """
@@ -53,3 +65,10 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    price1 = float(input("Enter price #1: "))
+    price2 = float(input("Enter price #2: "))
+    price3 = float(input("Enter price #3: "))
+    print("\nHere are your prices!\n")
+    print("Price #1: ${:>8.2f}".format(float(price1)))
+    print("Price #2: ${:>8.2f}".format(float(price2)))
+    print("Price #3: ${:>8.2f}".format(float(price3)))
